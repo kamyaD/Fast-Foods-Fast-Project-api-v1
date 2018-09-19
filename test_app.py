a33f7755ca493427133e61fc288ac54f3aba1e5b
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-19 08:49:46 +0300

    CH-#160609811 Created a test to ensure order is deleted

--- test_app.py
+++ test_app.py
@@ -31,6 +31,31 @@ class apiEndpoints(unittest.TestCase):
         output = client.post(
             '/api/v1/all_orders', data = self.new_orders)
         self.assertTrue(output.status_code, 201 )
+    
+    # Ensure status of an order is Updated
+    def test_editOrder(self):
+        client = app.test_client(self)
+        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
+        self.order= {'name':'Tea'}
+        self.new_orders= [order for order in orders if order['name']== ['coffee']]
+        self.orders[0]['name'] = self.order
+        output = client.put(
+            '/api/v1/all_orders' , data = self.new_orders)
+        self.assertTrue(output.status_code, 201)
+    
+    # Ensure  an order is deleted
+    def test_deleteOrder(self):
+        client = app.test_client(self)
+        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
+        self.new_orders=[order for order in orders if order['name']== ['coffee']]
+        self.orders.remove(self.orders[0])
+        return self.new_orders
+        output = client.delete(
+            '/api/v1/all_orders' , data = self.new_orders)
+        self.assertTrue(output.status_code, 200)
+        self.assertEqual(self.new_orders,[{'name':'Beaf'},{'name' : 'Milk'}])
+    
+
 
 
 
