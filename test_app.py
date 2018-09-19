1edfbd3ed0da1a2a64f1e7ccfdd7028c48b04e78
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-19 08:43:20 +0300

    CH-#160609754 Created a test to ensure new order is created

--- test_app.py
+++ test_app.py
@@ -21,6 +21,17 @@ class apiEndpoints(unittest.TestCase):
             '/api/v1/all_orders', content_type='html/text')
         self.assertEqual(output.status_code, 200)
 
+    
+    # Ensure that new order is created
+    def test_addOrder(self):
+        client = app.test_client(self)
+        self.orders = [{'name':'coffee'}, {'name':'Beaf'},{'name' : 'Milk'}]
+        self.order= {'name':'Bread'}
+        self.new_orders=orders.append(self.order)
+        output = client.post(
+            '/api/v1/all_orders', data = self.new_orders)
+        self.assertTrue(output.status_code, 201 )
+
 
 
 if __name__ == '__main__':
