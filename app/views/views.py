61f65c9c2d60bd4b71999c1f7df9d8a5a1e7b4a0
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-25 17:02:11 +0300

    [BG] Fixed errors in views file

--- app/views/views.py
+++ app/views/views.py
@@ -3,16 +3,16 @@ from flask import Flask, render_template, jsonify, request,session,flash,redirec
 
 app = Flask(__name__) #define app and telling flask that template folder is named v1
 api = Blueprint('api', __name__)
-app.register_blueprint(api, url_prefix='/api/v1')
+
 
 orders=[]
 special = "[@_!#$%^&*()<>?/\|}{~:]"
 
-@api.route('/tests', methods=['GET']) #Testing the jsonify out put on a browser
+@api.route('/orders', methods=['GET']) #Testing the jsonify out put on a browser
 def getOrders():
     return jsonify({'message' : 'Itworks!'})
 
-@api.route('/orders', methods=['POST']) # Places a new Order
+@api.route('/orders_post', methods=['POST']) # Places a new Order
 def addOrder():
     order_from_user = request.get_json()
     order = dict()
@@ -27,14 +27,14 @@ def addOrder():
     return jsonify({'orders' : orders})
     
 
-@api.route('/orders', methods=['GET']) # GET API that gets all orders
+@api.route('/orders_all', methods=['GET']) # GET API that gets all orders
 def returnAll():
     return jsonify({'orders': orders})
 
 
-@api.route('/orders<int:name>', methods=['PUT']) # Update the status of an order
+@api.route('/orders_put/<int:name>', methods=['PUT']) # Update the status of an order
 def editOrder(name):
-    for order in orders:
+    for i, order in enumerate(orders):
         if order['id']== name:
             order['name']=request.get_json()['name']
             order['price']=request.get_json()['price']
@@ -42,18 +42,19 @@ def editOrder(name):
     return jsonify({'order' : orders})
 
 
-@api.route('/orders<int:name>', methods=['GET']) # fetch Specific order
+@api.route('/orders_get/<int:name>', methods=['GET']) # fetch Specific order
 def returnOne(name):
-    for order in orders:
+    for i, order in enumerate(orders):
         if order['id']== name:
             return jsonify(order)
     return jsonify({"message":"Iten doesnt exist"})
 
 
-@api.route('/orders<int:name>', methods=['Delete']) # Delete an order
+@api.route('/orders_del/<int:name>', methods=['Delete']) # Delete an order
 def deleteOrder(name):
     for i, order in enumerate(orders):
         if order['id']== name:
             del orders[i]
     return jsonify({'orders': orders})
 
+app.register_blueprint(api, url_prefix='/api/v1')
