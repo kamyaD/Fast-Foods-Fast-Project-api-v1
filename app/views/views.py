3cff73ed142a918d72384943763ecf894a8559b4
kamya Domnick <33648833+kamyaD@users.noreply.github.com>
2018-09-27 20:36:04 +0300

    [BG] restructured directory

--- app/views/views.py
+++ app/views/views.py
@@ -1,18 +1,21 @@
 #import objects from the Flask model
 from flask import Flask, render_template, jsonify, request,session,flash,redirect,url_for, Blueprint 
 
-app = Flask(__name__) #define app and telling flask that template folder is named v1
+
+
+ #define app and telling flask that template folder is named v1
+app = Flask(__name__)
 api = Blueprint('api', __name__)
 
 
 orders=[]
 special = "[@_!#$%^&*()<>?/\\|}{~:]"
 
-@api.route('/orders', methods=['GET']) #Testing the jsonify out put on a browser
-def getOrders():
-    return jsonify({'message' : 'Itworks!'})
+# @api.route('/orders', methods=['GET']) #Testing the jsonify out put on a browser
+# def getOrders():
+#     return jsonify({'message' : 'Itworks!'})
 
-@api.route('/orders_post', methods=['POST']) # Places a new Order
+@api.route('/orders', methods=['POST']) # Places a new Order
 def addOrder():
     order_from_user = request.get_json()
     order = dict()
@@ -27,14 +30,14 @@ def addOrder():
     return jsonify({'orders' : orders})
     
 
-@api.route('/orders_all', methods=['GET']) # GET API that gets all orders
+@api.route('/orders', methods=['GET']) # GET API that gets all orders
 def returnAll():
     return jsonify({'orders': orders})
 
 
-@api.route('/orders_put/<int:name>', methods=['PUT']) # Update the status of an order
+@api.route('/orders/<int:name>', methods=['PUT']) # Update the status of an order
 def editOrder(name):
-    for i, order in enumerate(orders):
+    for order in orders:
         if order['id']== name:
             order['name']=request.get_json()['name']
             order['price']=request.get_json()['price']
@@ -42,19 +45,19 @@ def editOrder(name):
     return jsonify({'order' : orders})
 
 
-@api.route('/orders_get/<int:name>', methods=['GET']) # fetch Specific order
+@api.route('/orders/<int:name>', methods=['GET']) # fetch Specific order
 def returnOne(name):
-    for i, order in enumerate(orders):
+    for  order in orders:
         if order['id']== name:
             return jsonify(order)
     return jsonify({"message":"Iten doesnt exist"})
 
 
-@api.route('/orders_del/<int:name>', methods=['Delete']) # Delete an order
+@api.route('/orders/<int:name>', methods=['Delete']) # Delete an order
 def deleteOrder(name):
-    for i, order in enumerate(orders):
+    for order in orders:
         if order['id']== name:
-            del orders[i]
+            orders.remove(order)
     return jsonify({'orders': orders})
 
 app.register_blueprint(api, url_prefix='/api/v1')
